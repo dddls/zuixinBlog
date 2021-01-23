5ff3fa16269ad4c575e2bc5a0a81bc27b7157cc1
abcd <2146685093@qq.com>
2021-01-23 15:13:22 +0800

    用户信息接口配置成功

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+//实现用户提交信息，注册事项
+//response 相应
+//request 请求
+router.post('/addUser',(req,res,next) =>{
+  console.log(req.body);
+  //用户填写的表单信息可以通过req.body获取到
+})
+
 module.exports = router;
