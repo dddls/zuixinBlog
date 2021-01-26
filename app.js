ff5b87983c0659ff589b2011a0d0d62c58c90793
abcd <2146685093@qq.com>
2021-01-26 09:46:02 +0800

    修复了文章提交bug

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//实现路由
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/article', articleRouter);
